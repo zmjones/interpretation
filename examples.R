8b6eadead2a60a1e002deaf33a90f0a8d72a93d9
Zachary M. Jones <zmj@zmjones.com>
2017-05-10 15:52:35 -0400

    add true components to pd graph

--- examples.R
+++ examples.R
@@ -32,26 +32,36 @@ effects.names = sapply(effects, function(x) paste0(x, collapse = ":"))
 
 fit = randomForest(data, Fn, ntree = 1000, mtry = 2)
 mp = lapply(effects, function(u) marginalPrediction(data, u, c(25, 100), fit))
+mp[[1]]$truth = mp[[1]]$x
+mp[[2]]$truth = mp[[2]]$y^2
+mp[[3]]$truth = mp[[3]]$w * mp[[3]]$x
+mp[[4]]$truth = sin(mp[[4]]$z)
 names(mp) = effects.names
 mp = rbindlist(mp, fill = TRUE, idcol = "effect")
-truth = lapply(effects, function(u)
+noiseless = lapply(effects, function(u)
   marginalPrediction(data, u, c(25, 100), NULL, predict.fun = predict.fun)$preds)
-mp$truth = do.call("c", truth)
+mp$noiseless = do.call("c", noiseless)
 mp$x = factor(mp$x, label = paste0("x = ", c(-1, 0, 1)))
+names(mp)[3] = "randomForest"
 
+to.extract = c("randomForest", "truth", "noiseless")
 png("pd.png", 12, 8, "in", res = 1200)
 grid.arrange(
-  ggplot(mp[effect == "x", ], aes(x, preds, group = 1)) + geom_point() +
-    geom_point(aes(x, truth), color = "red") +
+  ggplot(melt(mp[effect == "x", c("x", to.extract), with = FALSE],
+    id.vars = "x", variable.name = "method"),
+    aes(x, value, color = method)) + geom_point() +
     labs(y = expression(hat(f)(x))),
-  ggplot(mp[effect == "y", ], aes(y, preds)) + geom_line() +
-    geom_line(aes(y, truth), color = "red") +
+  ggplot(melt(mp[effect == "y", c("y", to.extract), with = FALSE],
+    id.vars = "y", variable.name = "method"),
+    aes(y, value, color = method)) + geom_line() +
     labs(y = expression(hat(f)(y))),
-  ggplot(mp[effect == "w:x", ], aes(w, preds)) + facet_wrap(~ x) + geom_line() +
-    geom_line(aes(w, truth), color = "red") +
+  ggplot(melt(mp[effect == "w:x", c("w", "x", to.extract),
+    with = FALSE], id.vars = c("x", "w"), variable.name = "method"),
+    aes(w, value, color = method)) + facet_wrap(~ x) + geom_line() +
     labs(y = expression(hat(f)(w, x))),
-  ggplot(mp[effect == "z", ], aes(z, preds)) + geom_line() +
-    geom_line(aes(z, truth), color = "red") +
+  ggplot(melt(mp[effect == "z", c("z", to.extract), with = FALSE],
+    id.vars = "z", variable.name = "method"),
+    aes(z, value, color = method)) + geom_line() +
     labs(y = expression(hat(f)(z))),
   ncol = 2
 )
