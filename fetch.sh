1f073a8a823e5cf9c6c03f0165a05a63a8325b91
Zachary M. Jones <zmj@zmjones.com>
2017-05-18 14:00:06 -0400

    fix crime fetch script

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-mkdir -p ../data
-wget -b ../data/chicago.log -O ../data/chicago.csv https://data.cityofchicago.org/api/views/6zsd-86xi/rows.csv?accessType=DOWNLOAD&bom=true&query=select+*
+mkdir -p data
+wget -b data/chicago.log -O data/chicago.csv https://data.cityofchicago.org/api/views/6zsd-86xi/rows.csv?accessType=DOWNLOAD&bom=true&query=select+*
